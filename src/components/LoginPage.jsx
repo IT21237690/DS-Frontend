b9ad3972775fde6c679b7e1a2cfdae3c9ad77cd7
Bhayga Peramuna <it21241246@my.sliit.lk>
2024-05-14 00:20:57 +0530

    login page UI

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -37,13 +37,29 @@ const LoginPage = () => {
 
 
   return (
-    <div>
-      <h2>Login</h2>
-      {error && <p style={{ color: 'red' }}>{error}</p>}
-      <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} placeholder="Username" />
-      <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" />
-      <button onClick={handleLogin}>Login</button>
+    //  
+    <div class="grid lg:grid-cols-2 sm:grid:cols-1 items-center min-h-screen bg-gray-200 bg-gradient-to-r from-white to-blue-500">
+    <div class="  sm:flex ">
+      <img src="../../public/login.jpg" alt="E-learning platform" class="w-full h-full object-cover rounded-xl m-4" />
     </div>
+    <div class="w-full md:w-1/2 mx-auto p-8 m-4 space-y-4 bg-white rounded-xl drop-shadow-2xl backdrop-blur-md">
+      <h2 class="text-2xl font-semibold text-center text-gray-800">Welcome Back!</h2>
+      {error && <p class="text-red-500 text-sm text-center">{error}</p>}
+      <form class="flex flex-col space-y-4">
+        <div class="flex items-center border-b border-gray-200 py-2">
+          <i class="fas fa-user text-gray-400 mr-2"></i>  <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} placeholder="Username" class="w-full px-3 py-2 rounded-md focus:outline-none focus:ring focus:ring-blue-500 focus:ring-opacity-50" />
+        </div>
+        <div class="flex items-center border-b border-gray-200 py-2">
+          <i class="fas fa-lock text-gray-400 mr-2"></i>  
+          <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" class="w-full px-3 py-2 rounded-md focus:outline-none focus:ring focus:ring-blue-500 focus:ring-opacity-50"/>
+        </div>
+        <button type="submit" class="w-full py-2 px-4 bg-blue-500 text-white rounded-md hover:bg-blue-700 focus:outline-none focus:ring focus:ring-blue-500 focus:ring-opacity-50">Login</button>
+      </form>
+      <a href="#" class="text-sm text-center text-gray-600 hover:text-blue-500">Forgot Password?</a>
+    </div>
+  </div>
+  
+
   );
 };
 
