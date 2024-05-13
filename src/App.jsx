925b1bfa92881c578294d30a0566db4d7456ffda
Pasindu Serasinghe <IT21244834@my.sliit.lk>
2024-05-13 21:28:41 +0530

    Added routes

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,11 @@ import InsLoginPage from './components/instructor pages/InstructorLoginPage.jsx'
 import RegisterPage from './components/RegisterPage.jsx';
 import UserDetailsPage from './components/UserDetailsPage.jsx';
 import InsDetailsPage from './components/instructor pages/InstructorDetailsPage.jsx';
+import Home from "./Components/home pages/Home.jsx";
+import About from "./Components/home pages/About.jsx";
+import Work from "./Components/home pages/Work.jsx";
+import Contact from "./Components/home pages/Contact.jsx";
+import Footer from "./Components/home pages/Footer.jsx";
 import AllCourses from './components/AllCourses.jsx';
 import CreateComponent from './components/instructor pages/AddCourse.jsx';
 import UpdateComponent from './components/instructor pages/EditCourseDetails.jsx';
@@ -27,6 +32,13 @@ function App() {
             <Route path="/payment" element={<PaymentGateway/>} />
             {/* Define the route for the VideoPlayer component */}
             <Route path="/video-player" element={<VideoPlayer />} />
+
+            <Route path="/About" element={<About />} />
+            <Route path="/Home" element={<Home />} />
+            <Route path="/Work" element={<Work />} />
+            <Route path="/Contact" element={<Contact />} />
+            <Route path="/Footer" element={<Footer />} />
+            
         </Routes>
     </div>
   )
